ba0d712967f6df8d277e8e4ca16fc2c40c637a9b
arleneamericamartinez <hubacek1221>
2019-04-24 16:32:49 -0500

    finished tictactoe

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -7,8 +7,58 @@ var midright = document.getElementById("midright");
 var botleft = document.getElementById("botleft");
 var botmid = document.getElementById("botmid");
 var botright = document.getElementById("botright");
+var userscore =document.getElementById("userScore").innerHTML;
 
-var round = 1;
+var userwins = 0;
+
+var winningblocks = [
+    [topleft,topmid,topright],
+    [topleft,midmid,botright],
+    [topleft,midleft,botleft],
+    [topmid,midmid,botmid],
+    [topright,midright,botright],
+    [midleft,midmid,midright],
+    [botleft,botmid,botright],
+    [botleft,midmid,topright]
+];
+function checkifUserWon() {
+       for(var i = 0; i < winningblocks.length; i++){
+               if (winningblocks[i][0].innerHTML === "X"){
+                   if (winningblocks[i][1].innerHTML === "X"){
+                       if (winningblocks[i][2].innerHTML === "X"){
+                           userwins ++;
+                           document.getElementById("userScore").innerHTML = userwins;
+                           alert("You won");
+                           resetGame();
+                       }
+                   }
+               }
+       }
+}
+function checkifAiWon() {
+    for(var i = 0; i < winningblocks.length; i++){
+        if (winningblocks[i][0].innerHTML === "O"){
+            if (winningblocks[i][1].innerHTML === "O"){
+                if (winningblocks[i][2].innerHTML === "O"){
+                    alert("You Lost")
+                    resetGame();
+                }
+            }
+        }
+    }
+}
+
+function resetGame() {
+    usersblocks = [];
+    AIsblocks = [];
+    blocks=[topleft,topmid,topright,midleft,midmid,midright,botleft,botmid,botright];
+    blocks.forEach(function (e) {
+        e.innerHTML = "";
+    })
+}
+
+
+
 
 var blocks=[
     topleft,topmid,topright,midleft,midmid,midright,botleft,botmid,botright
@@ -22,6 +72,7 @@ function AIsTurn() {
     blocks[AisBlockChoice].innerHTML = "O";
     var tempblock = blocks.splice(AisBlockChoice,1);
     AIsblocks.unshift(tempblock);
+    checkifAiWon();
 }
 
 function isEven(num) {
@@ -34,6 +85,7 @@ function turn(block) {
         var tempblock = blocks.splice(blocks.indexOf(block),1);
         usersblocks.unshift(tempblock);
         console.log(usersblocks);
+        checkifUserWon();
         AIsTurn()
     }else{
         alert("Pick another square")
